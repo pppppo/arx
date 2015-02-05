d68e51d6ed9bbce704fefe546339f6a38182f163
Keehwan Park <keehwan.park@gmail.com>
2015-02-05 16:15:48 -0500

    Initial commit

---
+++ run_l_anonymizations.sh
@@ -0,0 +1,13 @@
+ARX_CLASSPATH=lib/colt/colt-1.2.0.jar:lib/commons/commons-io-2.4.jar:lib/commons/commons-lang-2.6.jar:lib/commons/commons-math3-3.1.1.jar:lib/junit/hamcrest-core-1.3.jar:lib/hppc/hppc-0.6.0.jar:lib/jama/Jama-1.0.2.jar:lib/junit/junit-4.11.jar:lib/jhc/libjhc-1.0.0-swt.jar:lib/log4j/log4j-1.2.17.jar:lib/mysql/mysql-connector-java-5.1.27-bin.jar:lib/objectselector/objectselector-0.1-lib.jar:lib/jface/org.eclipse.core.commands_3.6.2.v20130123-162658.jar:lib/jface/org.eclipse.core.runtime_3.8.0.v20120912-155025.jar:lib/draw2d/org.eclipse.draw2d_3.8.1.201301141834.jar:lib/jface/org.eclipse.equinox.common_3.6.100.v20120522-1841.jar:lib/jface/org.eclipse.jface_3.8.102.v20130123-162658.jar:lib/nattable/org.eclipse.nebula.widgets.nattable.core_1.1.0.201405012245.jar:lib/swtchart/org.swtchart.ext_0.8.0.v20120301.jar:lib/swtchart/org.swtchart_0.8.0.v20120301.jar:lib/poi/poi-3.10-FINAL-20140208.jar:lib/poi/poi-ooxml-3.10-FINAL-20140208.jar:lib/poi/poi-ooxml-schemas-3.10-FINAL-20140208.jar:lib/postgresql/postgresql-9.3-1101.jdbc41.jar:lib/sqlite/sqlite-jdbc-3.7.2.jar:lib/swt/swt-4.2.1-cocoa-macosx-x86_64.jar:lib/swt/swt-4.2.1-cocoa-macosx.jar:lib/swt/swt-4.2.1-gtk-linux-x86.jar:lib/swt/swt-4.2.1-gtk-linux-x86_64.jar:lib/swt/swt-4.2.1-win32-win32-x86.jar:lib/swt/swt-4.2.1-win32-win32-x86_64.jar:lib/swtknob/swtknob-1.0.0.jar
+
+javac -encoding UTF-8 -cp $ARX_CLASSPATH:build/classes -s src -d build/classes $(find src/teanon -name "*.java" -type f)
+echo "==============================================="
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity2
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity5
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity10
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity15
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity2WithSuppression
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity5WithSuppression
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity10WithSuppression
+java -cp $ARX_CLASSPATH:build/classes syntheticmedical.L3Diversity15WithSuppression
+
---
+++ src/teanon/syntheticmedical/L3Diversity10WithSuppression.java
@@ -0,0 +1,113 @@
+/*
+ * ARX: Powerful Data Anonymization
+ * Copyright (C) 2012 - 2014 Florian Kohlmayer, Fabian Prasser
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package syntheticmedical;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.deidentifier.arx.examples.Example;
+
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.AttributeType;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.AttributeType.Hierarchy.DefaultHierarchy;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.Data.DefaultData;
+import org.deidentifier.arx.DataSubset;
+import org.deidentifier.arx.criteria.DPresence;
+import org.deidentifier.arx.criteria.KAnonymity;
+import org.deidentifier.arx.criteria.RecursiveCLDiversity;
+import org.deidentifier.arx.metric.Metric;
+
+/**
+ * This class implements an example on how to apply the d-presence criterion.
+ *
+ * @author Fabian Prasser
+ * @author Florian Kohlmayer
+ */
+public class L3Diversity10WithSuppression extends Example {
+
+    /**
+     * Entry point.
+     * 
+     * @param args
+     *            the arguments
+     */
+    public static void main(final String[] args) {
+        int l = 10;
+        
+        try {
+            // Define public dataset
+            final Data data = Data.create("dataset/data.csv", '|');
+
+            // Set data attribute types
+            data.getDefinition().setAttributeType("id", AttributeType.INSENSITIVE_ATTRIBUTE);
+            data.getDefinition().setAttributeType("patient_id", AttributeType.INSENSITIVE_ATTRIBUTE);
+            data.getDefinition().setAttributeType("full_name", AttributeType.IDENTIFYING_ATTRIBUTE);
+            
+            data.getDefinition().setAttributeType("zip", Hierarchy.create("dataset/zip11.csv", '|'));
+            data.getDefinition().setAttributeType("yob", Hierarchy.create("dataset/yob.csv", '|'));
+            data.getDefinition().setAttributeType("gender", Hierarchy.create("dataset/gender.csv", '|'));
+            data.getDefinition().setAttributeType("visit_date", Hierarchy.create("dataset/visit_date.csv", '|'));
+            data.getDefinition().setAttributeType("hospital_name", Hierarchy.create("dataset/hospital_name.csv", '|'));
+            
+            data.getDefinition().setAttributeType("complaint", AttributeType.SENSITIVE_ATTRIBUTE);
+            data.getDefinition().setAttributeType("classification", AttributeType.INSENSITIVE_ATTRIBUTE);
+
+            // Create an instance of the anonymizer
+            final ARXAnonymizer anonymizer = new ARXAnonymizer();
+            final ARXConfiguration config = ARXConfiguration.create();
+            config.addCriterion(new KAnonymity(l));
+            config.addCriterion(new RecursiveCLDiversity("complaint", 3, l));
+            //config.setMaxOutliers(0d);
+            //config.setMaxOutliers(0.001d);
+            //config.setMaxOutliers(0.005d);
+            config.setMaxOutliers(0.01d);
+            //config.setMaxOutliers(0.02d);
+            config.setMetric(Metric.createLossMetric(Metric.AggregateFunction.ARITHMETIC_MEAN));
+            
+            // Now anonymize
+            final ARXResult result = anonymizer.anonymize(data, config);
+
+            // Print input
+            //System.out.println(" - Input data:");
+            //print(data.getHandle().iterator());
+
+            // Print info
+            printResult(result, data);
+            
+            // Print results
+            //System.out.println(" - Transformed data:");
+            //print(result.getOutput(false).iterator());
+            
+            // Write results
+            System.out.print(" - Writing data...");
+            result.getOutput(false).save("dataset/anonymized_flash_l10_sup0.01.csv", '|');
+            System.out.println("Done!");
+
+        } catch (final IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
